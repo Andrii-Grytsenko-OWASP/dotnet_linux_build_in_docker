173b5efefbe64eca1670156a74af25f096120fad
Andrii-Grytsenko-OWASP <90089469+Andrii-Grytsenko-OWASP@users.noreply.github.com>
2022-03-03 18:26:20 +0200

    Update run.cmd
    Change run.cmd
    The current folder detected authomatically inside run.cmd

--- run.cmd
+++ run.cmd
@@ -1,14 +1,8 @@
-IF "%1." == "." (
-  ECHO PRJ_HOME path should be defined. Path must be ended with "\"
-  GOTO EXIT
-) ELSE (
-  SET PRJ_HOME=%1
-)
-IF "%2" == "Release" OR "%2" == "Debug" (
-  SET PRJ_TARGET=%2
+SET PRJ_HOME=%cd%
+IF "%1" == "Release" OR "%1" == "Debug" (
+  SET PRJ_TARGET=%1
 ) ELSE (
   SET PRJ_TARGET=Release
 )
 SET PRJ_TARGET_SWITCH=-c %PRJ_TARGET%
-docker run --name example-server -v %PRJ_HOME%bin:/app/bin --rm example-dotnet %PRJ_TARGET_SWITCH%
-:EXIT
+docker run --name example-server -v %PRJ_HOME%\bin:/app/bin --rm example-dotnet %PRJ_TARGET_SWITCH%
